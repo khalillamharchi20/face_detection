f1274dfe3136bb9ce3ea7e539d79093894311ad7
khalillamharchi20 <khalillamharchi20@gmail.com>
2021-01-06 21:20:33 +0100

    third commit

--- khalil.py
+++ khalil.py
@@ -1,9 +1,12 @@
 import cv2
 trained_face_data=cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-img=cv2.imread('khalil.jpg')
-gray=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-face=trained_face_data.detectMultiScale(gray)
-cv2.rectangle(img,(277,101),(277+394,101+394),(0,0,255),2)
-cv2.imshow('khalil',img)
-cv2.waitKey()
-print('stop')
+webcam=cv2.VideoCapture(0)
+while True:
+    succes , frame =webcam.read()
+    gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    face_det=trained_face_data.detectMultiScale(gray)
+    for k in face_det:
+        (x,y,w,h)=k
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,0),2)
+    cv2.imshow('khalil',frame)
+    cv2.waitKey(1)
